afd056b40c94d7ab3b6af4458a5fb3c1dbfc6cfe
Jayden Alvarez <jayden.alvarez@tudent.csulb.edu>
2023-11-17 16:41:58 -0800

    3.2 Create Page

--- contacts/models.py
+++ contacts/models.py
@@ -2,8 +2,9 @@ from django.db import models
 from django.utils import timezone
 
 class Contact(models.Model):
-    name = models.CharField(max_length=100)
-    email = models.EmailField()
+    name = models.CharField(max_length=100, unique=True)
+    email = models.EmailField(unique=True)
+    notes = models.TextField(blank=True, null=True)
     created_time = models.DateTimeField(default=timezone.now)
 
     def __str__(self):
--- contacts/views.py
+++ contacts/views.py
@@ -1,4 +1,16 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .forms import ContactForm
 
 def start_page(request):
     return render(request, 'contacts/start_page.html')
+
+def create_contact(request):
+    if request.method == 'POST':
+        form = ContactForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('start_page')
+    else:
+        form = ContactForm()
+    
+    return render(request, 'contacts/create_contact.html', {'form': form})
